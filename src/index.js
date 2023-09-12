65f65b19b488eeb0edc61d4ca16902eadb2f33d2
Guibert Alexis <blysboutiquee@gmail.com>
2023-09-12 02:38:06 +0200

    level 2

--- src/index.js
+++ src/index.js
@@ -287,6 +287,27 @@ class SlabRoad extends React.Component{
         }
 
         const level2 = () => {
+            const nbSquareWidth = 12
+            const arrayOfAllBlobColors = ['red', 'blue', 'green', 'purple', 'orangered']
+            const matrixOfWall = [
+                [0, 0, 2, 0, 11], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 2, 0, 9], [2, 0, 0, 14, 0], [0, 0, 0, 0, 0], [2, 0, 0, 14, 0],
+                [0, 1, 3, 0, 5], [0, 3, 0, 5, 0], [0, 0, 3, 0, 5], [0, 0, 0, 0, 0], [1, 3, 0, 12, 0], [1, 0, 3, 0, 13], [0, 0, 3, 0, 13], [1, 2, 0, 11, 0], [2, 1, 0, 9, 0], [0, 0, 2, 0, 14], [1, 1, 5, 0, 99], [0, 0, 5, 0, 99],
+                [0, 0, 2, 0, 3], [0, 0, 0, 0, 0], [1, 1, 4, 0, 10, 3], [0, 3, 0, 13, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 2, 0, 8, 0], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0], [4, 1, 0, 10, 0], [0, 0, 0, 0, 0], [0, 0, 5, 0, 99],
+                [0, 1, 0, 0, 0], [0, 2, 0, 4, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 1, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 3, 0, 12], [0, 1, 3, 0, 12], [1, 0, 2, 0, 17], [0, 0, 5, 0, 99],
+                [0, 0, 0, 0, 0], [0, 0, 2, 0, 4], [0, 0, 0, 0, 0], [0, 0, 2, 0, 2], [0, 0, 0, 0, 0], [1, 2, 0, 2, 0], [1, 0, 2, 0, 8], [1, 1, 2, 0, 21], [0, 1, 0, 0, 0], [1, 0, 0, 0, 0], [2, 0, 0, 21, 0], [0, 0, 5, 0, 99],
+                [0, 2, 5, 3, 98], [0, 1, 5, 0, 98], [0, 3, 0, 1, 0], [0, 1, 3, 0, 1], [0, 1, 0, 0, 0], [3, 0, 0, 1, 0], [1, 1, 2, 0, 16], [1, 1, 0, 0, 0], [0, 3, 0, 20, 0], [1, 1, 3, 0, 20], [0, 0, 0, 0, 0], [0, 0, 3, 0, 20],
+                [0, 0, 5, 0, 98], [0, 0, 5, 0, 98], [0, 0, 3, 1, 1], [0, 0, 5, 0, 98], [0, 0, 0, 0, 0], [2, 0, 0, 18, 0], [1, 0, 0, 0, 0], [0, 2, 0, 16, 0], [0, 1, 0, 0, 0], [2, 1, 0, 18, 0], [0, 2, 0, 17, 0], [0, 1, 2, 0, 18],
+            ]
+
+            return {
+                matrixOfWall: doRealMatrixOfWall(matrixOfWall, nbSquareWidth),
+                nbSquareWidth: nbSquareWidth,
+                nbSquareHeight: matrixOfWall.length / nbSquareWidth,
+                arrayOfAllBlobColors: arrayOfAllBlobColors
+            }
+        }
+
+        const level3 = () => {
             const nbSquareWidth = 11
             const arrayOfAllBlobColors = ['red', 'blue', 'green', 'purple', 'orangered']
             const matrixOfWall = [
@@ -302,21 +323,6 @@ class SlabRoad extends React.Component{
                 [0, 0, 3, 0, 3], [0, 1, 0, 0, 0], [1, 2, 0, 4, 0], [1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 3, 0, 9, 0], [0, 1, 3, 0, 10], [0, 1, 3, 0, 10], [1, 0, 2, 0, 8], [0, 0, 0, 0, 0], [0, 0, 5, 0, 98],
                 [0, 1, 2, 0, 5], [0, 3, 0, 10, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
                 [0, 0, 2, 0, 6], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 3, 0, 9], [0, 0, 0, 0, 0], [0, 0, 3, 0, 9], [0, 0, 0, 0, 0], [2, 1, 0, 8, 0], [0, 3, 0, 7, 0], [0, 2, 0, 5, 0],
-            ]
-
-            return {
-                matrixOfWall: doRealMatrixOfWall(matrixOfWall, nbSquareWidth),
-                nbSquareWidth: nbSquareWidth,
-                nbSquareHeight: matrixOfWall.length / nbSquareWidth,
-                arrayOfAllBlobColors: arrayOfAllBlobColors
-            }
-        }
-
-        const level3 = () => {
-            const nbSquareWidth = 5
-            const arrayOfAllBlobColors = ['red', 'blue', 'green', 'purple', 'orangered']
-            const matrixOfWall = [
-                [0, 0, 5, 0, 98], [0, 0, 5, 0, 98], [0, 0, 5, 0, 98], [0, 0, 5, 0, 98], [0, 0, 5, 0, 98],
             ]
 
             return {
@@ -448,6 +454,13 @@ class SlabRoad extends React.Component{
                 />
             )
         }
+
+        // return(
+        //     <Game
+        //         levelProperties = {launchGame(3)}
+        //         retourMenu = {() => this.setState({currentDisplay: 'menu'})}
+        //     />
+        // )
     }
 }
 
